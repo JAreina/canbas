4e2208202eb838cc61fc0df4ab9a20e07663c36d
JAreina <jareinafdez@gmail.com>
2018-02-11 16:31:21 +0100

    documentation

--- canbas/modulos/crear-canva/createContainer.js
+++ canbas/modulos/crear-canva/createContainer.js
@@ -1,4 +1,14 @@
+
+
+/**
+ * Esta función crea un contenedor
+ * @param {string}  id del contenedor
+ * @param {string} etiqueta HTML del contenedor
+ * @param {string} appendTo dónde se añade el elemento creado
+ * @returns {element} el contenedor
+ */
 module.exports = function createContainer(id, tag, appendTo) {
+
 
   let containerCanvas = document.createElement(tag);
   containerCanvas.setAttribute("id", id);
--- docs/bundle.js
+++ docs/bundle.js
@@ -154,7 +154,17 @@ module.exports = function createCanvas(idContainer, idCanvas, width, height, bor
 /* 3 */
 /***/ (function(module, exports) {
 
+
+
+/*
+ * Esta función crea un contenedor 
+ * @param {string}  id del contenedor
+ * @param {string} etiqueta HTML del contenedor
+ * @param {string} a dónde se añade el elemento creado
+ * @returns {element} el contenedor
+ */
 module.exports = function createContainer(id, tag, appendTo) {
+
 
   let containerCanvas = document.createElement(tag);
   containerCanvas.setAttribute("id", id);
